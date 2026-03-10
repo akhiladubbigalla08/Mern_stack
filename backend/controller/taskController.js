42d57da42e5811021f7d11556ca4fdfe9d9b0149
akhiladubbigalla08 <akhila.dubbigalla56@gmail.com>
2026-03-10 11:33:37 +0530

    backend files

--- backend/controller/taskController.js
+++ backend/controller/taskController.js
@@ -14,3 +14,51 @@ exports.createTask = async (req, res) => {
         console.log(error);
     }
 }
+
+exports.getAllTask=async(req,res)=>{
+    try{
+        const tasks=await Task.find({user:req.user.id})
+        res.json(tasks)
+    }catch(error){
+        res.status(500).json(error)
+    }
+}
+exports.getTaskById=async(req,res)=>{
+    try{
+        const task=await Task.findOne({
+            _id:req.params.id,
+            user:req.user.id
+        })
+        if(!task){
+            res.status(404).json({msg:"Task not exist"})
+        }
+        res.json(task)
+}catch(error){
+    res.status(500).json(error)
+
+}
+}
+
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate(
+      { _id: req.params.id, user: req.user.id },
+      {
+        title: req.body.title,
+        description: req.body.description,
+        status: req.body.status
+      },
+      { new: true }
+    );
+
+    if (!task) {
+      return res.status(404).json({ msg: "Task not found" });
+    }
+
+    res.json(task);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+};
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {createTask}=require('../controller/taskController')
-const {protect}=require('../controller/middleware/authmiddleware')
+const {createTask, getAllTask,getTaskById,updateTask}=require('../controller/taskController')
+const {protect}=require('../middleware/authmiddleware')
 router.use(protect);
 router.post('/create',protect,createTask)
+router.get("/getTask",protect,getAllTask)
+router.get("/getTask/:id",protect,getTaskById)
+router.put("/updateTask/:id",protect,updateTask)
+
 module.exports = router;
